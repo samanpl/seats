02b1a4fd941b6e3f2cb37b5493cbb6ae81880777
EnAnsari <rahmat2022b@gmail.com>
2025-04-23 18:27:57 +0330

    update: rearranging seats

--- src/make-layout.py
+++ src/make-layout.py
@@ -5,18 +5,23 @@ y = 7 # columns
 data = [[0 for _ in range(y)] for _ in range(x)]
 
 curr = 1
-right = 1
+# right = 1
+# for i in range(x):
+#     if right:
+#         for j in range(y - 1, -1, -1):
+#             data[i][j] = curr
+#             curr += 1
+#     else:
+#         for j in range(y):
+#             data[i][j] = curr
+#             curr += 1
+#     right = 1 - right
+
 for i in range(x):
-    if right:
-        for j in range(y - 1, -1, -1):
-            data[i][j] = curr
-            curr += 1
-    else:
-        for j in range(y):
-            data[i][j] = curr
-            curr += 1
-    right = 1 - right
-        
+    for j in range(y):
+        data[i][j] = curr
+        curr += 1  
+
 try:
     with open('../layout.json', 'w') as json_file:
         json.dump(data, json_file)
